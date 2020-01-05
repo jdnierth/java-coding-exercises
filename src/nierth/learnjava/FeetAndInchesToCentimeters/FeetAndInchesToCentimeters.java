583fe01d3591250c2bc0e92fae4551290eaaaf1e
Jessica Nierth <jnierth@web.de>
2020-01-05 16:36:11 +0100

    feat (Added error handling)

--- src/nierth/learnjava/FeetAndInchesToCentimeters/FeetAndInchesToCentimeters.java
+++ src/nierth/learnjava/FeetAndInchesToCentimeters/FeetAndInchesToCentimeters.java
@@ -3,22 +3,29 @@ package nierth.learnjava.FeetAndInchesToCentimeters;
 public class FeetAndInchesToCentimeters {
     public static double calcFeedAndInchesToCentimeters(double feet, double inches) {
         if(feet < 0 || (inches < 0 || inches > 12)) {
+            System.out.println("Invalid value");
             return -1;
         }
 
         double feetInInches= feet * 12;
 
-        return (inches + feetInInches) * 2.54;
+        double centimeters = (inches + feetInInches) * 2.54;
+
+        System.out.println(feet + " feet, " +  inches + " inches = " + centimeters + " cm");
+        return centimeters;
     }
 
     public static double calcFeedAndInchesToCentimeters(double inches) {
         if(inches < 0) {
+            System.out.println("Invalid value");
             return -1;
         }
 
         double feet = inches / 12;
-        double remainingFeet = (int) inches % 12;
+        double remainingInches = (int) inches % 12;
 
-        return calcFeedAndInchesToCentimeters(feet, remainingFeet);
+        System.out.println(inches + " inches is equal to: " + feet + " feet and " + remainingInches + " remaining " +
+                "inches");
+        return calcFeedAndInchesToCentimeters(feet, remainingInches);
     }
 }
