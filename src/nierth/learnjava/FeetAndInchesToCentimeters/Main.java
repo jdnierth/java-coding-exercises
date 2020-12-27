d5e553bc7f6881ff7351428a01d5408cb4646b73
Jessica Nierth <jnierth@web.de>
2020-12-27 19:11:06 +0100

    feat (Feet and inches to centimeter calculation)

---
+++ src/nierth/learnjava/FeetAndInchesToCentimeters/Main.java
@@ -0,0 +1,16 @@
+package nierth.learnjava.FeetAndInchesToCentimeters;
+
+public class Main {
+    public static void main(String[] args) {
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(-10);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(0);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(1,0);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(20,0);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(42, 0);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(8, 4);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(100);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(156);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(157);
+        FeetAndInchesToCentimeters.calcFeedAndInchesToCentimeters(13, 1);
+    }
+}
