7685069d67f5eab2813c7f818623b68c2ee33274
Jessica Nierth <jnierth@web.de>
2020-12-28 11:08:21 +0100

    feat (Added exercise MintuesToYearsAndDaysCalculator)

---
+++ src/nierth/learnjava/MinutesToYearsAndDaysCalculator/Main.java
@@ -0,0 +1,11 @@
+package nierth.learnjava.MinutesToYearsAndDaysCalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(-10);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(561600);
+    }
+}
---
+++ src/nierth/learnjava/MinutesToYearsAndDaysCalculator/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,16 @@
+package nierth.learnjava.MinutesToYearsAndDaysCalculator;
+
+public class MinutesToYearsAndDaysCalculator {
+    public static void printYearsAndDays(long minutes) {
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+
+            long years = minutes / (365 * 24 * 60);
+
+            long days = (minutes / (24 * 60)) % 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
