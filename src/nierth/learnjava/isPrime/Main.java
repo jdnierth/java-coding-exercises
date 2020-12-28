653da0bdf7d8cfbf7ae49695dba9b794b8a73ed0
Jessica Nierth <jnierth@web.de>
2020-12-28 12:44:19 +0100

    feat (Added exercise isPrimeNumber)

---
+++ src/nierth/learnjava/isPrime/Main.java
@@ -0,0 +1,7 @@
+package nierth.learnjava.isPrime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPrime.printIsPrime(10));
+    }
+}
---
+++ src/nierth/learnjava/isPrime/readme.md
@@ -0,0 +1,3 @@
+# isPrime number
+* Create a method that generates a prime number
+* A prime number can be divided evenly either by one or by itself
