942e41e76cffc4ac25cc66980abb802a2b153191
Jessica Nierth <jnierth@web.de>
2020-12-28 13:09:59 +0100

    feat (Added sum3And5 challenge)

---
+++ src/nierth/learnjava/Sum3And5Challenge/Sum3AndChallenge.java
@@ -0,0 +1,28 @@
+package nierth.learnjava.Sum3And5Challenge;
+
+public class Sum3AndChallenge {
+
+    public static int sum3And5() {
+        int sum = 0;
+        int max = 5;
+        int count = 0;
+
+        for(int i = 1; i <= 1000; i++) {
+            if(canBeDivided(i)) {
+                count ++;
+                sum = sum + i;
+                System.out.println("Found number = " + i);
+
+                if(count == max) {
+                    break;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private static boolean canBeDivided(int a) {
+        return a % 3 == 0 && a % 5 == 0;
+    }
+}
