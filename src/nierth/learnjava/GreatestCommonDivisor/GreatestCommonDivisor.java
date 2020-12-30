1bb380014a9f920fefe63c5bbef2a6df6de467f1
Jessica Nierth <jnierth@web.de>
2020-12-30 16:10:20 +0100

    fix (Logical error in GreatestCommonDivisor)

--- src/nierth/learnjava/GreatestCommonDivisor/GreatestCommonDivisor.java
+++ src/nierth/learnjava/GreatestCommonDivisor/GreatestCommonDivisor.java
@@ -4,7 +4,7 @@ public class GreatestCommonDivisor {
     public static int getGreatestCommonDivisor(int first, int second) {
         int greatestCommonDivisor = 0;
 
-        if(first <= 0 || second <= 0) {
+        if(first < 10 || second < 10) {
             return -1;
         }
 
--- src/nierth/learnjava/GreatestCommonDivisor/Main.java
+++ src/nierth/learnjava/GreatestCommonDivisor/Main.java
@@ -2,6 +2,7 @@ package nierth.learnjava.GreatestCommonDivisor;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(10, 35));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(-20, 15));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(18, 9));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(25, 15));
