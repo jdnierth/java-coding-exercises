1f89d8328b4fdd70c991c9e40d42cfbf05583e2f
Jessica Nierth <jnierth@web.de>
2020-12-31 16:47:25 +0100

    feat (Added exercise MinimumAndMaximumChallenge)

---
+++ src/nierth/learnjava/MinimumAndMaximumChallenge/MinimumAndMaximumChallenge.java
@@ -0,0 +1,36 @@
+package nierth.learnjava.MinimumAndMaximumChallenge;
+
+import java.util.Scanner;
+
+public class MinimumAndMaximumChallenge {
+    public static void readingInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int input;
+
+        while(count < 4) {
+            System.out.println("Enter number " +  (count + 1) + ": ");
+            boolean hasInt = scanner.hasNextInt();
+
+            if(hasInt) {
+                input = scanner.nextInt();
+
+                min = (input < min) ? input : min;
+                max = (input > max) ? input : max;
+
+                count++;
+            } else {
+                System.out.println("Invalid Number");
+            }
+
+            scanner.nextLine();
+        }
+
+        System.out.println("Your min number: " + min + " your max number: " + max);
+
+        scanner.close();
+    }
+}
---
+++ src/nierth/learnjava/MinimumAndMaximumChallenge/readme.md
@@ -0,0 +1,8 @@
+# Minimum And Maximum Challenge
+* Read the numbers from the console entered by the user
+* Print the minimum and maximum the user has entered
+* Before the user enters the number, print the message `Enter number:`
+* If the user enters an invalid number, break out of the loop and print the minimum and maximum number
+
+# Tip
+* Use an endless `while` loop
