cb738daf89d9db0e41f8fb36630dea69e08585ce
Jessica Nierth <jnierth@web.de>
2021-01-01 18:18:12 +0100

    feat (Adding exercise Point)

---
+++ src/nierth/learnjava/Point/Point.java
@@ -0,0 +1,49 @@
+package nierth.learnjava.Point;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(){
+        int differenceXsquare = (int) Math.pow(getX(),2);
+        int differenceYsquare = (int) Math.pow(getY(),2);
+        return Math.sqrt(differenceXsquare + differenceYsquare);
+    }
+    public double distance(int x, int y){
+        int differenceXsquare = (int) Math.pow(getX() - x,2);
+        int differenceYsquare = (int) Math.pow(getY() - y,2);
+        return Math.sqrt(differenceXsquare + differenceYsquare);
+    }
+    public double distance(Point z){
+        int differenceXsquare = (int) Math.pow(getX() - z.x,2);
+        int differenceYsquare = (int) Math.pow(getY() - z.y,2);
+        return Math.sqrt(differenceXsquare + differenceYsquare);
+    }
+
+}
