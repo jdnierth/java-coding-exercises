ab1d8ca3890583705baca5b4113d966bd311494c
Jessica Nierth <jnierth@web.de>
2021-01-02 12:51:57 +0100

    feat (Added exercise ComplexOperations)

---
+++ src/nierth/learnjava/ComplexOperations/ComplexNumber.java
@@ -0,0 +1,37 @@
+package nierth.learnjava.ComplexOperations;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber number) {
+        add(number.getReal(), number.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        subtract(number.getReal(), number.getImaginary());
+    }
+}
