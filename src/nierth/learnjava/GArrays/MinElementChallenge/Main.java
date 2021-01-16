059b193bb45ddebdb9d85fa6977e577f6e5dc072
Jessica Nierth <jnierth@web.de>
2021-01-16 15:36:46 +0100

    feat (Added challenge MinElementChallenge)

---
+++ src/nierth/learnjava/GArrays/MinElementChallenge/Main.java
@@ -0,0 +1,16 @@
+package nierth.learnjava.GArrays.MinElementChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter how many numbers: ");
+        int count = scanner.nextInt();
+        int[] numbers = MinElementChallenge.readIntegers(count);
+        int min = MinElementChallenge.findMin(numbers);
+        System.out.println("The minimum number is: " + min);
+    }
+}
---
+++ src/nierth/learnjava/GArrays/MinElementChallenge/MinElementChallenge.java
@@ -0,0 +1,36 @@
+package nierth.learnjava.GArrays.MinElementChallenge;
+
+import java.util.Scanner;
+
+public class MinElementChallenge {
+
+    public static int[] readIntegers(int count)  {
+        Scanner scanner = new Scanner(System.in);
+        int currentNumber = 0;
+        int[] numbers = new int[count];
+
+        while(currentNumber < count) {
+            System.out.println("Get next number " + currentNumber + ":");
+            numbers[currentNumber] = (scanner.nextInt());
+            currentNumber ++;
+        }
+
+        return numbers;
+    }
+
+    public static int findMin(int[] numbers) {
+        int min = 0;
+
+        if(numbers.length == 0) {
+            return min;
+        }
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        return min;
+    }
+}
